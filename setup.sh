b0c46aaaab65a69b36cfdff16ef51e4e8de24eca
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2014-10-20 10:34:44 +0200

    add firefox, vagrant and more

--- setup.sh
+++ setup.sh
@@ -14,12 +14,15 @@ brew install caskroom/cask/brew-cask
 
 # install apps
 brew cask install dropbox
+brew cast install adium
 
 # install apps for dev
 brew install macvim --override-system-vim
 brew cask install virtualbox
 brew cask install google-chrome
+brew cask install firefox
 brew cask install launchrocket
+brew cask install vagrant
 
 # make macvim the new default vim
 echo export PATH='/usr/local/bin:$PATH' >> ~/.bash_profile
@@ -29,9 +32,18 @@ brew install cmake
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh
 
+brew install tmux
+brew install reattach-to-user-namespace
+
 brew install redis
 brew install selecta
 brew install node
+
+#
+# npm
+#
+npm install -g grunt
+npm install -g bower
 
 #
 # .dotfiles
